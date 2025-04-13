6a589e2f18075c0e59341eb180012632ee1650b5
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-04-13 08:32:37 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 /**
- * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
- * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
- *
- * English:
- * ! Please do not change the below code, it is very important for the project.
- * It is my motivation to maintain and develop the project for free.
- * ! If you change it, you will be banned forever
- * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
+ * Goat Bot Render Deployment Fix by Eren
  */
 
+const express = require("express");
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
+// === Express server to keep Render service alive ===
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.send("Goat Bot is alive and running on Render!");
+});
+
+app.listen(PORT, () => {
+	console.log(`✅ Server running at http://localhost:${PORT}`);
+});
+
+// === Start the Goat bot process ===
 function startProject() {
 	const child = spawn("node", ["Goat.js"], {
 		cwd: __dirname,
@@ -28,7 +27,7 @@ function startProject() {
 	});
 
 	child.on("close", (code) => {
-		if (code == 2) {
+		if (code === 2) {
 			log.info("Restarting Project...");
 			startProject();
 		}
