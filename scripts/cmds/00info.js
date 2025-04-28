490974ca127e9032f6abf00248074ab44c2d7fbf
prime-eren99 <ovhi.bbz108@outlook.com>
2025-04-28 09:53:48 +0600

    Update 00info.js

--- scripts/cmds/00info.js
+++ scripts/cmds/00info.js
@@ -1,4 +1,4 @@
-#const fs = require('fs');const moment = require('moment-timezone');
+const fs = require('fs');const moment = require('moment-timezone');
 module.exports = {
   config: {
     name: "info",
