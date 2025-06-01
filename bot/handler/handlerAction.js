10e825b75b35c081e1e5312244e5bd567e11a027
Evaan <ovhi.bbz108@outlook.com>
2025-06-01 16:06:23 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61574046213712"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61576212342334"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
